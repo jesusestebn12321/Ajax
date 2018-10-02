3bb978d7924461b0e231a41ed61bc017b72af453
Estebn12321 <jesusesteban12321@gmail.com>
2018-10-02 16:54:05 -0400

    arreglos

--- js/main.js
+++ js/main.js
@@ -1,39 +1,38 @@
 $(document).ready(function(){
-		$('#practicas').click(function(){
-			var nombre= $('#nombre');
-			var descripcion= $('#descripcion');
-			var categoria= $('#categoria');
-			if (isNotEmpty(nombre) && isNotEmpty(descripcion) && isNotEmpty(categoria)) {
-				$.ajax({
-					url: 'controllers.php',
-					method: 'POST',
-					dataType: 'text',
-					data: {
-						key: 'add',
-						nombre: nombre.val(),
-						descripcion: descripcion.val(),
-						categoria: categoria.val()
-					}, success: function (response) {
-						swal(response , {
-							icon:"success",
-						});
-						$('#formulario')[0].reset();
-					}
-				});
-			}else{
-				swal('todos los campos son requeridos',{
-					icon:"info",
-				});
-			}
-		});
-		
-	});
-		
-	function upData(id){
+	$('#practicas').click(function(){
 		var nombre= $('#nombre');
 		var descripcion= $('#descripcion');
 		var categoria= $('#categoria');
-		var id=id;
+		if (isNotEmpty(nombre) && isNotEmpty(descripcion) && isNotEmpty(categoria)) {
+			$.ajax({
+				url: 'controllers.php',
+				method: 'POST',
+				dataType: 'text',
+				data: {
+					key: 'add',
+					nombre: nombre.val(),
+					descripcion: descripcion.val(),
+					categoria: categoria.val()
+				}, success: function (response) {
+					swal(response , {
+						icon:"success",
+					});
+					$('#formulario')[0].reset();
+				}
+			});
+		}else{
+			swal('todos los campos son requeridos',{
+				icon:"info",
+			});
+		}
+	});		
+});
+		
+function upData(id){
+	var nombre= $('#nombre');
+	var descripcion= $('#descripcion');
+	var categoria= $('#categoria');
+	var id=id;
 			// if (isNotEmpty(nombre) && isNotEmpty(descripcion) && isNotEmpty(categoria)) {
 			// 	$.ajax({
 			// 		url: 'upDate.php',
@@ -53,54 +52,48 @@ $(document).ready(function(){
 			// }else{
 			// 	alert('Todos los Campos son Requridos');
 			// }
-	}
-	function destroy(id) {
-			swal({
-				title: "¿Desea eliminar este registro?",
-				text: "el registro se eliminara en caso de que precione OK!",
-				icon: "warning",
-				buttons: true,
-				dangerMode: true,
-				customClass: 'wow tada'
-			  })
-			  .then((willDelete) => {
-				if (willDelete) {
-					$.ajax({
-						url: 'destroy.php',
-						method: 'POST',
-						dataType: 'text',
-						data: {
-							key: 'destroy',
-							id: id
-						}, success: function (response) {
-							swal(response, {
-								icon: "success",
-							});
-							
-							$('#producto'+id).css('display','none');
-						}
-					});
-				}else{
-				  swal("El registro esta a salvo!",{
-					  icon:"info",
-				  });
+}
+function destroy(id) {
+	swal({
+		title: "¿Desea eliminar este registro?",
+		text: "el registro se eliminara en caso de que precione OK!",
+		icon: "warning",
+		buttons: true,
+		dangerMode: true,
+		customClass: 'wow tada'
+	}).then((willDelete) => {
+		if (willDelete) {
+			$.ajax({
+				url: 'destroy.php',
+				method: 'POST',
+				dataType: 'text',
+				data: {
+					key: 'destroy',
+					id: id
+				},Success: function (response) {
+					swal(response, {
+						icon: "success",
+					});	
+					$('#producto'+id).css('display','none');
 				}
-			  });
-
-	}
-
-	function isNotEmpty(request) {
-		if (request.val() == '') {
-			request.css('border', '1px solid red');
-			return false;
-		} else{
-			request.css('border', '');
-			return true;
+			});
+		}else{
+			swal("El registro esta a salvo!",{
+				icon:"info",
+			});
 		}
+	 });
+}
+function isNotEmpty(request) {
+	if (request.val() == '') {
+		request.css('border', '1px solid red');
+		return false;
+	}else{
+		request.css('border', '');
+		return true;
 	}
+}
 
-
-	
 $('[data-toggle="tooltip"]').tooltip();
 wow = new WOW(
 {
@@ -111,9 +104,3 @@ wow = new WOW(
 	}
 }
 );
-wow.init();
-document.getElementById('moar').onclick = function() {
-	var section = document.createElement('section');
-	section.className = 'section--purple wow fadeInDown';
-	this.parentNode.insertBefore(section, this);
-};
